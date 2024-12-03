60cc6786149e44716656d6fd2d30d4870ed8dfed
Joshua Holland <jholland@shipt.com>
2024-12-03 14:08:57 -0600

    cleanup

--- solutions/day1.go
+++ solutions/day1.go
@@ -7,6 +7,22 @@ import (
 	"slices"
 )
 
+func Day1Part1() {
+	input1, input2, err := utils.GetInputs("./data/lists.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	diff := 0
+
+	for i := range input1 {
+		diff += utils.Abs(input1[i] - input2[i])
+	}
+
+	fmt.Println(diff)
+}
+
 func Day1Part2() {
 	input1, input2, err := utils.GetInputs("./data/lists.txt")
 
